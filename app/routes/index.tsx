696328dcf424565c6bf90024367abcf2914900cd
kirke <kirke@kirkenoMacBook-Air.local>
2022-06-06 00:35:31 +0900

    set env

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,14 @@
+import firebaseConfig from "~/components/firebase/config";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
 export let meta = () => {
   return {
     title: "Remix Secrets App",
   };
 };
+
 const Index = () => {
   return (
     <div className="remix__page">
